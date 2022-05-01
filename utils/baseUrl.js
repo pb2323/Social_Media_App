98cc989e00972fcca21f11f3073483b6534adf29
Puneet Bajaj <pbajaj0023@gmail.com>
2022-05-01 20:46:19 +0530

    deploy setup

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://642d-103-136-89-0.in.ngrok.io";
+    : "https://mini-instagram-india.herokuapp.com";
 
 module.exports = baseUrl;
