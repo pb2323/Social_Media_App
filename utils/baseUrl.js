d8e551ae2c53c1da6f38c19842383524d0864902
Puneet Bajaj <pbajaj0023@gmail.com>
2022-05-08 01:34:52 +0530

    update host name to network-in

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : process.env.HOST_NAME;
+    : "https://network-in.herokuapp.com";
 
 module.exports = baseUrl;
