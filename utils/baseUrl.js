ac770131fa127c77f7899504f4eec909bd503388
Puneet Bajaj <pbajaj0023@gmail.com>
2022-05-08 01:18:18 +0530

    update host name to process env

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mini-instagram-india.herokuapp.com";
+    : process.env.HOST_NAME;
 
 module.exports = baseUrl;
