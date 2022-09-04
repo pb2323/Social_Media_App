11ea8597efcace718a8d5cfb42a09e81daa55733
Puneet Bajaj <pbajaj0023@gmail.com>
2022-09-04 16:46:19 +0530

    Fix dedicated ipfs subdomains issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   env: {
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dwd17olsk/image/upload",
-    INFURA_IPFS_URL: "https://ipfs.infura.io:5001/api/v0/add?pin=false"
+    INFURA_IPFS_URL: "https://ipfs.infura.io:5001/api/v0/add?pin=false",
+    INFURA_IPFS_PROJECT_ID: "9051a6c8843375390d670e609c262793",
+    INFURA_IPFS_SECRET_KEY: "9051a6c8843375390d670e609c262793",
+    BASIC_AUTH_INFURA: "Basic MkE5UGJubnJ5MDR1VDNHMDJQcDlpQkIycDZHOjkwNTFhNmM4ODQzMzc1MzkwZDY3MGU2MDljMjYyNzkz",
+    DEDICATED_GATEWAY_INFURA: "network-in.infura-ipfs.io"
   },
 };
--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -4,12 +4,13 @@ const uploadPic = async (media) => {
   try {
     const form = new FormData();
     form.append("file", media);
+    // form.append("Authorization", process.env.BASIC_AUTH_INFURA)
     // form.append("upload_preset", "social_media_app");
     // form.append("cloud_name", "indersingh");
 
-    const res = await axios.post(process.env.INFURA_IPFS_URL, form);
+    const res = await axios.post(process.env.INFURA_IPFS_URL, form, { headers: { "Authorization": process.env.BASIC_AUTH_INFURA } });
     console.log(res);
-    return `https://infura-ipfs.io/ipfs/${res.data.Hash}`;    //Comment if using cloudinary 
+    return `https://${process.env.DEDICATED_GATEWAY_INFURA}/ipfs/${res.data.Hash}`;    //Comment if using cloudinary 
   } catch (error) {
     console.log(error.message);
     return;
